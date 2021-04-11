a8090c96bc24c409f16b7f08e0a6a21be518125f
Lewisdev0 <82328986+Lewisdev0@users.noreply.github.com>
2021-04-11 19:44:21 +0300

    Update erkekkayıt.js

--- komutlar/erkekkayıt.js
+++ komutlar/erkekkayıt.js
@@ -3,9 +3,7 @@ const db = require('wio.db');
 exports.run = (client, message, args) => {
     const misafir = message.guild.roles.cache.find(r => r.id === "KAYITSIZ ID"); 
     const kayıterkek = message.guild.roles.cache.find(r => r.id === "ERKEK ID"); 
-  
-  const log = message.guild.channels.cache.find(c => c.id === "LOG ID"); 
-  const tag = "TAG";
+    const tag = "TAG";
   if(!message.member.roles.cache.array().filter(r => r.id === "KAYIT YETKILISI ID")[0]) { 
     return message.channel.send("YETKİN YOK MK APTALI");
   } else {
@@ -14,40 +12,21 @@ exports.run = (client, message, args) => {
     const c = message.guild.member(member)
     const nick = args[1];
     const yas = args[2];
-      if(!nick) return message.channel.send("**Kayıt Edebilmem İçin Bir ``İsim`` Girmelisin.**")
-      if(!yas) return message.channel.send("**Kayıt Edebilmem İçin Bir ``Yaş`` Girmelisin.**")
+      if(!nick) return message.channel.send("**Kayıt Edebilmem İçin Bir İsim Girmelisin.**")
+      if(!yas) return message.channel.send("**Kayıt Edebilmem İçin Bir Yaş Girmelisin.**")
     c.roles.add(kayıterkek)
     c.roles.remove(misafir)
     c.setNickname(`${tag} ${nick} , ${yas}`)
      const yolla = new Discord.MessageEmbed()
     .setDescription(`
-
-• Kaydı Yapılan Üye: **${c.user.tag}**
-• Değiştirilen İsim: ${tag} ${nick} | ${yas}
-• Verilen Rol: ${kayıterkek}
+Kaydı Yapılan Üye: **${c.user.tag}**
+Değiştirilen İsim: ${tag} ${nick} | ${yas}
+Verilen Rol: ${kayıterkek}
 `)
          .setFooter('BOT ADI Erkek Kayıt Sistemi')
      message.channel.send(yolla)
      
-    
-
-    const lewis = new Discord.MessageEmbed()
-    .setDescription(`
-**Bir Erkek Üye Kayıt Oldu! Aşağıda Bilgiler Yazmakta.**
-
-• Kaydı Yapılan Üye: **${c.user.tag}**
-• Kaydı Yapan Yetkili: **${message.author.tag}**
-• Değiştirilen İsim: ${tag} ${nick} | ${yas}
-
-**Rol Bilgileri Aşağıda Yazmakta.**
-
-• Alınan Rol: ${misafir}
-• Verilen Rol: ${kayıterkek}
-
-`)
-    .setFooter('BOT ADI Erkek Kayıt Sistemi')
-   
-    log.send(lewis)
+  
   }
 }
 exports.conf = {
